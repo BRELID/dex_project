cd1336d00fb2606bbdcb8403ea8bad83b12417c3
BRELID <brahim.relid@gmail.com>
2022-11-13 15:02:17 +0100

    4) Add transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -30,26 +30,6 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value)
-        public
-        returns (bool success)
-    {
-        // Require that sender has enough tokens to spend
-        require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
-
-        // Deduct tokens from spender
-        balanceOf[msg.sender] -= _value;
-
-        //Credit tokens to receiver
-        balanceOf[_to] += _value;
-
-        // Emit Event
-        emit Transfer(msg.sender, _to, _value);
-
-        return true;
-    }
-
     function approve(address _spender, uint256 _value)
         public
         returns (bool success)
@@ -59,6 +39,51 @@ contract Token {
         allowance[msg.sender][_spender] = _value;
 
         emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // Require that sender has enough tokens to spend
+        require(balanceOf[msg.sender] >= _value);
+        _transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
+        require(_to != address(0));
+
+        // Deduct tokens from spender
+        balanceOf[_from] -= _value;
+
+        //Credit tokens to receiver
+        balanceOf[_to] += _value;
+
+        // Emit Event
+        emit Transfer(_from, _to, _value);
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        // Check approval and the size of the value
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        // Reset Allowance
+        allowance[_from][msg.sender] -= _value;
+
+        // Spend tokens
+        _transfer(_from, _to, _value);
         return true;
     }
 }
